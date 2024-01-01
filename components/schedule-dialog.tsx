7cd77ea0bdaa0d7c79e8227fb9cad9b7197d8468
Kinfe123 <kinfishtech@gmail.com>
2024-01-01 14:48:40 +0300

    ping: prod db pulling

--- components/schedule-dialog.tsx
+++ components/schedule-dialog.tsx
@@ -190,7 +190,6 @@ function ScheduleUpload({ serverSesion, jobPosition }: any) {
              
             />
           </div>
-
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="description" className="text-right">
               Description
