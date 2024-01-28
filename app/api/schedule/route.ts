9bbf381f731e495f71b1c77f30f920ec415bafcb
Kinfe123 <kinfishtech@gmail.com>
2024-01-28 17:15:18 +0300

    scheduling fix

--- app/api/schedule/route.ts
+++ app/api/schedule/route.ts
@@ -1,4 +1,5 @@
 import { prisma } from "@/lib/db";
+import { RequiredKeys } from "@prisma/client/runtime/library";
 import { title } from "process";
 import * as z from "zod";
 
@@ -15,9 +16,10 @@ export async function POST(req: Request) {
     if (!body) {
       return new Response("No body provided", { status: 404 });
     }
-    const { title, description, date, authorId, time , endTime , completed } = body;
+    const { title, description, date, authorId, time, endTime, completed } =
+      body;
 
-    console.log('THe body is:' , body)
+    console.log("THe body is:", body);
 
     const schedule = await prisma.schedule.create({
       data: {
@@ -27,18 +29,39 @@ export async function POST(req: Request) {
         endTime: endTime,
         description: description,
         title: title,
-        completed:completed,
-
+        completed: completed,
       },
-    }); 
+    });
 
-    console.log("tHE SCHIDULED one: " , schedule)
-
-
+    console.log("tHE SCHIDULED one: ", schedule);
 
     return new Response(JSON.stringify(schedule));
   } catch (err) {
-    console.log("Error has occured while scheduling... ," , err);
+    console.log("Error has occured while scheduling... ,", err);
     return new Response("Error has occured", { status: 404 });
+  }
+}
+
+export async function PATCH(req: Request) {
+  try {
+    const body = await req.json();
+
+    const { id, status } = body;
+
+    
+    const res = await prisma.schedule.update({
+      where: {
+        id: id,
+      },
+      data: {
+        completed: !status,
+      },
+    });
+
+    return new Response(JSON.stringify(res));
+  } catch (err) {
+    return new Response("Error has occured while patching the request", {
+      status: 404,
+    });
   }
 }
