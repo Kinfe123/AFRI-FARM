699af9b2196b7abeea1e2b7b45b20f2e0129e1a5
Kinfe123 <kinfishtech@gmail.com>
2024-01-28 23:42:16 +0300

    fix: ui fix

--- app/dashboard/progress/page.tsx
+++ app/dashboard/progress/page.tsx
@@ -1,5 +1,11 @@
 import { prisma } from "@/lib/db"
 
+
+export const metadata = {
+    title: "Progress",
+    description: "Track your progress   ",
+  };
+  
 const Progress = async () => {
     const tasks = await (await prisma.schedule.findMany({})).filter(r => r.completed)
     console.log('The task lenght is: ' , tasks)
