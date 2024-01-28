c974087f7073572b53c0e8e69db6de2c59d46c8b
Kinfe123 <kinfishtech@gmail.com>
2024-01-28 18:29:31 +0300

    sprinter issue fix

--- app/dashboard/upload/page.tsx
+++ app/dashboard/upload/page.tsx
@@ -1,4 +1,3 @@
-
 import { redirect } from "next/navigation";
 
 import { DashboardHeader } from "@/components/header";
@@ -42,6 +41,7 @@ import { prisma } from "@/lib/db";
 import DownloadPage from "../components/download";
 import { timeAgo } from "@/lib/utils";
 import { DotsHorizontalIcon } from "@radix-ui/react-icons";
+import Resources from "../components/resources";
 export const metadata = {
   title: "Upload",
   description: "Upload Resource",
@@ -58,23 +58,19 @@ export default async function SettingsPage() {
     redirect("/login");
   }
 
-  const handleDelete = async (id:any) => {
-
-    const req = await fetch("/api/upload/"  , {
-      method:"DELETE",
+  const handleDelete = async (id: any) => {
+    const req = await fetch("/api/upload/", {
+      method: "DELETE",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
         id: id,
-      })
-
-
-    })
-    const res = await req.json()
-    console.log('The reponse :' , res)
-
-  }
+      }),
+    });
+    const res = await req.json();
+    console.log("The reponse :", res);
+  };
 
   return (
     <DashboardShell>
@@ -87,101 +83,7 @@ export default async function SettingsPage() {
           </div>
         </div>
 
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-          {resources.map((r) => {
-            return (
-              //   <div key={r.id} className="flex flex-col gap-2  items-start rounded-2xl border-[0.112px] border-none h-[300px] bg-gradient-to-tr from-purple-400/10 via-transparent to-transparent">
-
-              //      <div>
-
-              //      <h1 className="text-muted-foreground font-sans">Test header</h1>
-              //      <p>Test description goes here for moer clarification</p>
-              //      <div className="flex justify-between items-center">
-              //         <p>7 min ago</p>
-              //         <button>
-              //             download
-              //         </button>
-              //      </div>
-              //      </div>
-              //   </div>
-              <Card
-                key={r.id}
-                className="rounded-3xl relative  bg-gradient-to-tr from-purple-400/10 via-transparent to-transparent"
-              >
-                <CardHeader className="grid grid-cols-[1fr_10px] items-start gap-4 space-y-0 relative">
-                  <div className="space-y-3">
-                    <CardTitle>{r.Title}</CardTitle>
-
-                    <CardDescription>{r.description}</CardDescription>
-                  </div>
-                </CardHeader>
-                <CardContent>
-                  <div className="absolute top-2 right-2">
-                    <DropdownMenu>
-                      <DropdownMenuTrigger asChild>
-                        <Button
-                          variant="ghost"
-                          className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
-                        >
-                          <DotsHorizontalIcon className="h-4 w-4" />
-                          <span className="sr-only">Open menu</span>
-                        </Button>
-                      </DropdownMenuTrigger>
-                      <DropdownMenuContent align="end" className="w-[160px]">
-                        <DropdownMenuItem>Edit</DropdownMenuItem>
-                        <DropdownMenuItem>Make a copy</DropdownMenuItem>
-                        <DropdownMenuItem>Favorite</DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        {/* <DropdownMenuSub>
-                          <DropdownMenuSubTrigger>
-                            Labels
-                          </DropdownMenuSubTrigger>
-                          <DropdownMenuSubContent>
-                            <DropdownMenuRadioGroup value={task.label}>
-                              {labels.map((label) => (
-                                <DropdownMenuRadioItem
-                                  key={label.value}
-                                  value={label.value}
-                                >
-                                  {label.label}
-                                </DropdownMenuRadioItem>
-                              ))}
-                            </DropdownMenuRadioGroup>
-                          </DropdownMenuSubContent>
-                        </DropdownMenuSub> */}
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>
-                          Delete
-                          <DropdownMenuShortcut>⌘⌫</DropdownMenuShortcut>
-                        </DropdownMenuItem>
-                      </DropdownMenuContent>
-                    </DropdownMenu>
-                  </div>
-                  <div className="flex space-x-4 text-sm text-muted-foreground">
-                    <div className="flex items-center">
-                      <CircleIcon className="mr-1 h-3 w-3 fill-sky-400 text-sky-400" />
-                      {r.type}
-                    </div>
-                    <div className="flex items-center">
-                      <StarIcon className="mr-1 h-3 w-3" />
-                      20k
-                    </div>
-                    <br />
-                  </div>
-                  <div className="flex mt-2 justify-between text-muted-foreground">
-                    <div className="flex items-center space-x-1 rounded-md bg-transparent text-secondary-foreground ">
-                      <DownloadPage url={r.resourceUrl} />
-                      <Separator orientation="vertical" className="h-[20px]" />
-                    </div>
-                    <div className="">
-                      <p>{timeAgo(r.updatedAt)}</p>
-                    </div>
-                  </div>
-                </CardContent>
-              </Card>
-            );
-          })}
-        </div>
+        <Resources resources={resources} />
       </div>
     </DashboardShell>
   );
